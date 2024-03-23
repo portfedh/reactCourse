2de6058a4f93f200586ecc539ccafaa596512ac1
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-03-23 09:40:24 -0600

    Full Star Raiting

--- 08 usepopcorn/src/StarRaiting.js
+++ 08 usepopcorn/src/StarRaiting.js
@@ -24,6 +24,7 @@ const starStyle = {
 
 export default function StarRaiting({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRaiting, SetTempRaiting] = useState(0);
   return (
     <div style={containerStyle}>
       <div>
@@ -31,18 +32,26 @@ export default function StarRaiting({ maxRating = 5 }) {
           <Star
             key={i}
             onRate={() => setRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRaiting ? tempRaiting >= i + 1 : rating >= i + 1}
+            onHoverIn={() => SetTempRaiting(i + 1)}
+            onHoverOut={() => SetTempRaiting(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRaiting || ""}</p>
     </div>
   );
 }
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -70,22 +79,3 @@ function Star({ onRate, full }) {
     </span>
   );
 }
-
-/*
-EMPTY STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="#000"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg>
-
-*/
