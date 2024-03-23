3768f8d3d283a7655c6edf1d748b777bfb0ed264
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-03-23 11:24:05 -0600

    StarRating component

--- 08 usepopcorn/src/index.js
+++ 08 usepopcorn/src/index.js
@@ -2,12 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
-import StarRaiting from "./StarRaiting";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Test() {
+  const [movieRating, setMovieRating] = React.useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRaiting maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
+    />
+    <StarRating size={24} defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
